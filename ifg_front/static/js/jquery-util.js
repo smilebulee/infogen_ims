5dea7901d1bb249a0dfc2c8a1b26999d572f90ee
smilebulee <bulee@infogen.co.kr>
2020-03-13 18:23:03 +0900

    44

--- ifg_front/static/js/jquery-util.js
+++ ifg_front/static/js/jquery-util.js
@@ -1,3 +1,29 @@
+var g_dialog;
+var g_modal;
+var g_toast;
+
+$(document).ready(function(){
+    g_dialog = new ax5.ui.dialog({
+        title: '',
+        lang:{
+            "ok": "확인", "cancel": "취소"
+        }
+    });
+
+    g_modal = new ax5.ui.modal({
+        onStateChanged: function () {
+
+        }
+    });
+
+    g_toast = new ax5.ui.toast({
+        containerPosition: "top-right",
+        onStateChanged: function(){
+
+        }
+    });
+});
+
 (function($){
 	$.fn.initGrid = function(opts){
 		var options = $.extend({}, $.fn.initGrid.defaultOpts, opts);
@@ -39,15 +65,46 @@
 })(jQuery);
 
 (function($){
+	$.ajaxCall = function(url, method, param, callbackFn, global){
+	    var data;
+	    if(typeof param == 'string') data = param;
+	    else data = JSON.stringify(param);
+
+	    var ajaxOpts = {
+	        method: method,
+            url: url,
+            data: data,
+            dataType: 'json',
+            error: function(jqXHR, textStatus, errorThrown ){
+                dialog.setConfig({
+                    title: 'Error!!'
+                });
+
+                dialog.alert(textStatus);
+            },
+            success: function(data, textStatus, jqXHR){
+                if(typeof callbackFn == 'function') callbackFn;
+	            else if(typeof callbackFn == 'string') eval(callbackFn);
+            }
+	    }
+	    if(global != undefined && typeof global == 'boolean') ajaxOpts.global = global;
+	    if(global != undefined && (global == 'true' || global == 'false')) ajaxOpts.global = (global == 'true');
+
+        $.ajax(ajaxOpts);
+
+	};
+})(jQuery);
+
+(function($){
 	$.fn.method = function(opts){
 		var options = $.extend({}, $.fn.method.defaultOpts, opts);
-		
+
 		return this.each(function(){
 					var $el = $(this);
 			   });
 	};
-	
+
 	$.fn.method.defaultOpts = {
-	
+
 	};
 })(jQuery);
