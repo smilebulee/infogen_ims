57db3b5bf89a4b60b419505e330da966a2827ca2
BabyFox <lovernd@naver.com>
2020-03-16 17:23:13 +0900

    test

--- ifg_front/emp_api/urls.py
+++ ifg_front/emp_api/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.urls import path
 from . import views
 
 from django.conf import settings
@@ -8,6 +9,8 @@ app_name = 'emp_api'
 
 urlpatterns = [
     url(r'^$', views.Emp_api_index.as_view(), name='emp_api'),
+    url(r'^testFox/', views.Emp_api_testFox.as_view(), name='emp_foxTest'),
+    url(r'^testFox_ajax/get', views.testFox_ajax, name='testFox_ajax'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ifg_front/emp_api/views.py
+++ ifg_front/emp_api/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.views import View
 from django.views import generic
+from django.http import JsonResponse
 import requests
+import logging
+import json
+
+logger = logging.getLogger(__name__)
 
 class Emp_api_index(generic.TemplateView):
     def get(self, request, *args, **kwargs):
@@ -15,3 +20,33 @@ class Emp_api_index(generic.TemplateView):
         }
         
         return render(request, template_name, rr)
+
+
+class Emp_api_testFox(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'emp/testFox.html'
+
+        r = requests.get('http://emp_api:5000/hello')
+        rr = {
+            "result": r.text
+        }
+
+        return render(request, template_name, rr)
+
+def testFox_ajax(request):
+
+    param = json.loads(request.GET['param'])
+
+    data = {
+        'email' : param['email'],
+        'password' : param['password'],
+        'addr' : param['state'] + ' ' + param['city'] + ' ' + param['addr'] + ' ' + param['addrDetl'] + param['zip'],
+        'sex' : param['chk']
+    }
+
+    r = requests.get('http://emp_api:5000/save',data)
+
+
+
+    logger.info(data)
+    return JsonResponse(data)
