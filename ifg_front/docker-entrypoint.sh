fde7a503bcf0f8931bf63223a0feb7cdb12402f6
smilebulee <61953914+smilebulee@users.noreply.github.com>
2020-03-19 21:30:26 +0900

    333

--- ifg_front/docker-entrypoint.sh
+++ ifg_front/docker-entrypoint.sh
@@ -8,10 +8,10 @@
 dockerize -wait tcp://db:5432 -timeout 20s
 
 # Apply database migrations
-echo "Apply database migrations"
+#echo "Apply database migrations"
 python manage.py makemigrations
 python manage.py migrate
 
 # Start server
-echo "Starting server"
+#echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
