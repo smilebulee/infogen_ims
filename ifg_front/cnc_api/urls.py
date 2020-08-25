7cd86ecb71d844a430b8a69df497a42e7b673448
JeYoung-Lee <ljy00256@gmail.com>
2020-08-25 20:53:40 +0900

    front - postgresql  model 호출 ( cnc 참조)
    front - postgresql  model 호출 ( cnc 참조)

--- ifg_front/cnc_api/urls.py
+++ ifg_front/cnc_api/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required
 from . import views
+from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,7 +9,9 @@ from django.conf.urls.static import static
 app_name = 'cnc_api'
 
 urlpatterns = [
-    url(r'^$', views.Cnc_api_index.as_view(), name='cnc_api'),
+    path('', login_required(views.Cnc_api_index.as_view()), name='cnc_api'),
+    path('test2/', views.test2, name='test2'),
+    path('getTestview/', views.getTestview, name='getTestview'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
