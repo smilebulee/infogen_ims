6af85d77268c699955197d24a17632987c32f6dd
JeYoung-Lee <ljy00256@gmail.com>
2020-08-25 20:58:01 +0900

    Revert "front - postgresql  model 호출 ( cnc 참조)"
    This reverts commit 7cd86ecb71d844a430b8a69df497a42e7b673448.

--- ifg_front/cnc_api/views.py
+++ ifg_front/cnc_api/views.py
@@ -1,39 +1,12 @@
-from django.shortcuts import render, redirect
-# Create your views here.
+from django.shortcuts import render
 
+# Create your views here.
 from django.views import View
 from django.views import generic
-from django.http import JsonResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from .models import Tb_tpt
-from main.helpers import ajax_login_required
-
 import requests
-import logging
-import json
 
-logger = logging.getLogger(__name__)
 class Cnc_api_index(generic.TemplateView):
     def get(self, request, *args, **kwargs):
         template_name = 'cnc/index.html'
 
         return render(request, template_name)
-
-@login_required
-def test2(request):
-    logger.info("============test===================")
-    template_name = 'cnc/test.html'
-
-    return render(request, template_name)
-
-def getTestview(request):
-    param = json.loads(request.GET['param'])
-
-    #쿼리 조회 CNC_API_TB_TPT Table 조회
-    qs = Tb_tpt.objects.all().values('test_id','text_data','reg_user')
-    logger.info(qs)
-    logger.info(list(qs))
-    data = {
-        'list': list(qs)
-    }
-    return JsonResponse(data)
