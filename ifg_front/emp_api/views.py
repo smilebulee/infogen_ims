7d8d1610a5fb78b457f0bec8197e06ee3ee10986
poket04 <41422640+poket04@users.noreply.github.com>
2020-08-26 22:31:22 +0900

    emp - mariadb 연결 진행(참고용)
    소스는 정리 하여 다시한번 올려 드리겠습니다.
    해당 mariadb 연결 부분만 확인하시면 될것같습니다.

--- ifg_front/emp_api/views.py
+++ ifg_front/emp_api/views.py
@@ -5,7 +5,11 @@ from django.views import View
 from django.views import generic
 from django.http import JsonResponse
 
+from django.core.paginator import Paginator
+#from .models import Tb_page
+
 from django.views.decorators.csrf import csrf_exempt
+# from .models import TB_EMP,Cd
 
 import requests
 import logging
@@ -40,12 +44,13 @@ class Emp_api_testFox(generic.TemplateView):
 def insert_ajax(request):
 
     param = json.loads(request.POST['param'])
-
+    logger.info(param)
     datas = {
         'email' : param['email'],
         'password' : param['password'],
         'addr' : param['state'] + ' ' + param['city'] + ' ' + param['addr'] + ' ' + param['addrDetl'] + param['zip'],
         'sex' : param['chk']
+
     }
     logger.info('request.post : ' + request.POST['param'])
     logger.info(datas)
@@ -90,4 +95,172 @@ def search_ajax(request):
     logger.info(r.json())
     logger.info(json.loads(r.text))
     # return JsonResponse(r.json())
+    return JsonResponse(r.json(), safe=False)
+
+## 신규 추가 
+class Emp_api_testFox2(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'emp/testFox2.html'
+
+        r = requests.get('http://emp_api:5001/hello')
+        rr = {
+            "result": r.text
+        }
+
+        return render(request, template_name, rr)
+## 서치 이메일 or 아이디 검색으로 그리드 호출
+def newSearch_ajax(request) :
+    logger.info(request)
+    param = json.loads(request.GET['param'])
+    logger.info("new Search Test")
+    datas = {
+        'email': param['searchemail']
+    }
+    logger.info(datas)
+    r = requests.get('http://emp_api:5001/search', params=datas)
+    logger.info(r)
+    logger.info(r.text)
+    logger.info("----------------")
+    logger.info(r.json())
+    logger.info(json.loads(r.text))
+    # return JsonResponse(r.json())
+    return JsonResponse(r.json(), safe=False)
+
+def idCheck(request) :
+    logger.info("IDCHECK")
+    logger.info(request)
+
+    data2 = request.GET['id1']
+    datatest = {
+        'id' : data2
+    }
+    logger.info("data??")
+    logger.info(datatest)
+    logger.info(data2)
+
+    #logger.info('request.post : ' + request.POST['param'])
+    # logger.info(datas)
+    r = requests.get('http://emp_api:5001/idChektest', params=datatest)
+    logger.info(r)
+    logger.info(r.text)
+    logger.info(r.json())
+
+    return JsonResponse(r.json(), safe=False)
+
+## 신규 추가
+class Emp_api_testPark(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'emp/testPark.html'
+
+        r = requests.get('http://emp_api:5001/hello')
+        rr = {
+            "result": r.text
+        }
+
+        return render(request, template_name, rr)
+
+@csrf_exempt
+def insert_ajax_new(request):
+
+    param = json.loads(request.POST['param'])
+
+    datas = {
+        'id' : param['id1'],
+        'email' : param['email'],
+        'password' : param['password'],
+        'phone' : param['phone']
+
+    }
+    logger.info('request.post : ' + request.POST['param'])
+    logger.info(datas)
+    r = requests.post('http://emp_api:5001/signUp', data=datas)
+    logger.info(r)
+    logger.info(r.text)
+    logger.info(r.json())
+    return JsonResponse(r.json())
+
+def newSearch(request) :
+    logger.info(request)
+    param = json.loads(request.GET['param'])
+    logger.info("new Search Test")
+    datas = {
+        'id' : param['inputId'],
+        'email': param['searchemail'],
+        'password' :  param['inputSearchPw']
+    }
+    logger.info(datas)
+    r = requests.get('http://emp_api:5001/search2', params=datas)
+    logger.info(r)
+    logger.info(r.text)
+    logger.info("----------------")
+    logger.info(r.json())
+    logger.info(json.loads(r.text))
+    # return JsonResponse(r.json())
+    return JsonResponse(r.json(), safe=False)
+
+def empReference(request):
+    template_name = 'emp/empReference.html'
+
+    return render(request, template_name)
+
+def getPage(request) :
+    # init data (
+    param = json.loads(request.GET['param'])
+    logger.info("new Search Test333333")
+    datas = {
+        'id': param['page'],
+    }
+    logger.info(datas)
+    # 공통 코드마스터 데이터 조회
+    #06.23 test 다른 모델 장고 접속확인
+    qs = Tb_page.objects.all().order_by('page_id').values('page_id','page_nm','job_dv','remark')
+    logger.info(list(qs))
+    r = requests.get('http://emp_api:5001/empReferenceInit', params=datas)
+    paginator = Paginator(r.json(), 10)
+    logger.info("----------------")
+    logger.info(paginator)
+    logger.info(r)
+    logger.info(r.text)
+    logger.info("----------------")
+
+    result = paginator.get_page(param['page'])
+    logger.info(list(result.object_list))
+
+    logger.info(result)
+    data = {
+        'list': list(result.object_list),
+        'total_records': paginator.count,
+        'total_pages': paginator.num_pages,
+        'page': result.number,
+        'has_next': result.has_next(),
+        'has_prev': result.has_previous()
+    }
+    logger.info(data)
+    logger.info("dadadadaata")
+
+    #logger.info(r.json())
+    #logger.info(json.loads(r.text))
+    # return JsonResponse(r.json())
+    return JsonResponse(data)
+
+class mariatest(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        template_name = 'emp/mariatest.html'
+        #화면 호출
+        r = requests.get('http://emp_api:5001/hello')
+        rr = {
+            "result": r.text
+        }
+
+        return render(request, template_name, rr)
+
+def getMaria(request):
+    param = json.loads(request.GET['param'])
+
+    #api 호출
+    r = requests.get('http://emp_api:5001/mariatestDB')
+    logger.info(r)
+    logger.info(r.text)
+    logger.info(r.json())
+    logger.info(json.loads(r.text))
     return JsonResponse(r.json(), safe=False)
