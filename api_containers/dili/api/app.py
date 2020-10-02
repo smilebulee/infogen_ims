ebc2b9d2445af56a875063047f2cc97433c58b9b
yubin1020 <yubin1020@gmail.com>
2020-10-02 16:39:40 +0900

    결재요청
    결재요청

--- api_containers/dili/api/app.py
+++ api_containers/dili/api/app.py
@@ -22,6 +22,7 @@ class DateTimeEncoder(JSONEncoder):
         if isinstance(obj, (datetime.date, datetime.datetime)):
             return obj.isoformat()
 
+
 def dateConverter(param):
     if isinstance(param, datetime.datetime):
         return param.__str__()
@@ -349,14 +350,17 @@ class apvlReqHist(Resource): # Mariadb 연결 진행
         try:
             with mysql_con.cursor(pymysql.cursors.DictCursor) as cursor:
                 # 쿼리문 실행
-                sql = "SELECT *, CASE WHEN APVL_REQ_DIVS = '01' THEN '휴일근무' " \
-                    + "               WHEN APVL_REQ_DIVS = '02' THEN '야근근무' " \
-                    + "               WHEN APVL_REQ_DIVS = '03' THEN '연차결재'  " \
-                    + "               ELSE '' END APVL_REQ_NM  " \
-                    + "        , CASE WHEN TH1_APRV_STUS != '' AND TH2_APRV_STUS = '' THEN '미승인'" \
-                    + "               WHEN TH1_APRV_STUS != '' AND TH2_APRV_STUS != '' THEN '승인'" \
+                sql = "SELECT B.EMP_NAME" \
+                    + "       , CASE WHEN A.APVL_REQ_DIVS = '01' THEN '휴일근무' " \
+                    + "              WHEN A.APVL_REQ_DIVS = '02' THEN '야근근무' " \
+                    + "              WHEN A.APVL_REQ_DIVS = '03' THEN '연차결재'  " \
+                    + "              ELSE '' END APVL_REQ_NM  " \
+                    + "        , CASE WHEN A.TH1_APRV_STUS != '' AND A.TH2_APRV_STUS = '' THEN '미승인'" \
+                    + "               WHEN A.TH1_APRV_STUS != '' AND A.TH2_APRV_STUS != '' THEN '승인'" \
                     + "               ELSE '미승인' END APRV_STUS_NM" \
-                    + "  FROM TB_APVL_REQ_MGMT_M WHERE EMP_EMAL_ADDR = '" + data["email"] + "'"
+                    + "   FROM TB_APVL_REQ_MGMT_M A, TB_EMP_MGMT B" \
+                    + "  WHERE A.EMP_EMAL_ADDR = B.EMP_EMAIL" \
+                    + "    AND A.EMP_EMAL_ADDR = '" + data["email"] + "'"
                 logging.debug("apvlReqHist SQL문" + sql)
                 cursor.execute(sql)
 
@@ -372,64 +376,40 @@ class apvlReqHist(Resource): # Mariadb 연결 진행
 
         return json.dumps(result2, indent=4, cls=DateTimeEncoder)
 
-class noticeLst(Resource): # Mariadb 연결 진행
+class apvlReqHistDetl(Resource): # Mariadb 연결 진행
     def get(self):
-        logging.debug("noticeLst start")
-        logging.debug(request.get_json())
-        # get data
-        dept = request.args.get('dept')
-        name = request.args.get('name')
-        division = request.args.get('division')
-        skilKind = request.args.get('skilKind')
-        skil = request.args.get('skil')
-        category = request.args.get('category')
-        searchStr = request.args.get('searchStr')
 
-        logging.debug('---------------SEARCH---------------')
-        logging.debug('dept : ' + dept)
-        logging.debug('name : ' + name)
-        logging.debug('division : ' + division)
-        logging.debug('skilKind : ' + skilKind)
-        logging.debug('skil : ' + skil)
-        logging.debug('category : ' + category)
-        logging.debug('searchStr : ' + searchStr)
-        logging.debug('------------------------------------')
+        data = request.get_json()
 
         #requirements pymysql import 후 커넥트 사용
         mysql_con = pymysql.connect(host='218.151.225.142', port=3306, db='IFG_IMS', user='ims2', password='1234',
                                         charset='utf8')
         try:
             with mysql_con.cursor(pymysql.cursors.DictCursor) as cursor:
-                #쿼리문 실행
-                #sql = "SELECT TIT, CNTN, KD_DIVS_CD, MJR_YN, POP_OPEN_YN, DATA_INPT_ID, DATA_INPT_PGM_ID, DATA_UPD_ID, DATA_UPD_PGM_ID FROM TB_STTS_POST_MGMT_M "
-                sql = "SELECT A.POST_ID, A.TIT, A.CNTN, A.KD_DIVS_CD, A.MJR_YN, A.POP_OPEN_YN, A.DATA_INPT_ID, B.EMP_NAME, DATE_FORMAT(A.DATA_INPT_DTTM, '%Y-%M-%D %H:%I:%S') AS DATA_INPT_DTTM, A.DATA_INPT_PGM_ID, A.DATA_UPD_ID, DATE_FORMAT(A.DATA_UPD_DTTM, '%Y-%M-%D %H:%I:%S') AS DATA_UPD_DTTM, A.DATA_UPD_PGM_ID " \
-                        "FROM TB_STTS_POST_MGMT_M A LEFT OUTER JOIN TB_EMP_MGMT B ON A.DATA_INPT_ID = B.EMP_ID " \
-                        "WHERE 1=1 "
-                if searchStr != "":
-                    if category == "00":
-                        sql += "AND TIT LIKE '%" + searchStr + "%' "
-                    if category == "01":
-                        sql += "AND CNTN LIKE '%" + searchStr + "%' "
-                    if category == "02":
-                        sql += "AND (TIT LIKE '%" + searchStr + "%' OR CNTN LIKE '%" + searchStr + "%') "
-                    if category == "03":
-                        sql += "AND B.EMP_NAME LIKE '%" + searchStr + "%' "
-                sql += "ORDER BY POST_ID DESC "
-                logging.debug(sql)
+                # 쿼리문 실행
+                sql = "SELECT A.TH1_APRV_NM" \
+                    + "      ,A.TH2_APRV_NM" \
+                    + "      ,CASE WHEN A.TH1_APRV_STUS != '' THEN '승인'" \
+                    + "            ELSE '미승인' END TH1_APRV_STUS_NM" \
+                    + "      ,CASE WHEN A.TH2_APRV_STUS != '' THEN '승인'" \
+                    + "            ELSE '미승인' END TH2_APRV_STUS_NM" \
+                    + "   FROM TB_APVL_REQ_MGMT_M A, TB_EMP_MGMT B" \
+                    + "  WHERE A.EMP_EMAL_ADDR = B.EMP_EMAIL" \
+                    + "    AND A.EMP_EMAL_ADDR = '" + data["email"] + "'"
+                logging.debug("apvlReqHist SQL문" + sql)
                 cursor.execute(sql)
-                
+
         finally:
             mysql_con.close()
 
         result2 = cursor.fetchall()
-        #for row in result2:
-        #    logging.debug('====== row====')
-        #    logging.debug(row)
-        #    logging.debug('===============')
-        #array = list(result2)  # 결과를 리스트로
+        for row in result2:
+            logging.debug('====== row====')
+            logging.debug(row)
+            logging.debug('===============')
+        array = list(result2)  # 결과를 리스트로
 
-        return result2
-
+        return json.dumps(result2, indent=4, cls=DateTimeEncoder)
 
 api.add_resource(Hello, '/hello')
 api.add_resource(Register, '/register')
@@ -442,8 +422,7 @@ api.add_resource(yryMgmt,'/yryMgmt') #api 선언
 api.add_resource(wrkApvlReq,'/wrkApvlReq') #api 선언
 api.add_resource(saveApvlReq,'/saveApvlReq') #api 선언
 api.add_resource(apvlReqHist,'/apvlReqHist') #api 선언
-api.add_resource(noticeLst,'/noticeLst') #api 선언
-
+api.add_resource(apvlReqHistDetl,'/apvlReqHistDetl') #api 선언
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5006, debug=True)
--- ifg_front/dili_api/views.py
+++ ifg_front/dili_api/views.py
@@ -252,3 +252,18 @@ def getApvlReqHist(request):
     logger.info(logger.info(ast.literal_eval(r.json())))
     logger.info(json.loads(r.text))
     return JsonResponse(ast.literal_eval(r.json()), safe=False)
+
+def getApvlReqHistDetl(request):
+    param = json.loads(request.GET['param'])
+
+    logger.info("Parameters Start")
+    logger.info(param)
+    logger.info("Parameters End")
+
+    # api 호출
+    r = requests.get('http://dili_api:5006/apvlReqHistDetl', json=param)
+    logger.info(r)
+    logger.info(r.text)
+    logger.info(logger.info(ast.literal_eval(r.json())))
+    logger.info(json.loads(r.text))
+    return JsonResponse(ast.literal_eval(r.json()), safe=False)
