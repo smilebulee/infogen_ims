97dea51d21049e962d72b8a6549150b603d39cfb
shjo <sung32da@naver.com>
2020-10-07 11:21:36 +0900

    근무시간관리_연차결재창 모달연결
    근무시간관리_연차결재창 모달연결 업로드

--- ifg_front/static/js/modal.js
+++ ifg_front/static/js/modal.js
@@ -1,33 +1,42 @@
-(function($) {
-    $.popup = function(opt) {
-        $.ajax({
-            url: opt.url,
-            data: opt.data,
-            dataType: 'html',
-            success: function(html) {
-                $("<div class='modal_popup'>" + html + '</div>')
-                    .appendTo('body')
-                    .modal({
-                        escapeClose: false,
-                        clickClose: false,
-                        closeExisting: false
+            function modal(id) {
+                var zIndex = 9999;
+                var modal = $('#' + id);
+            
+                // 모달 div 뒤에 희끄무레한 레이어
+                var bg = $('<div>')
+                    .css({
+                        position: 'fixed',
+                        zIndex: zIndex,
+                        left: '0px',
+                        top: '0px',
+                        width: '100%',
+                        height: '100%',
+                        overflow: 'auto',
+                        // 레이어 색갈은 여기서 바꾸면 됨
+                        backgroundColor: 'rgba(0,0,0,0.4)'
                     })
-                    // 창뜰때 애니메이션 : jackInTheBox
-                    .addClass('animated faster')
-                    .addClass(opt.animation || 'jackInTheBox')
-                    .on($.modal.CLOSE, function(event, modal) {
-                        if (opt.close) opt.close(modal.result);
-
-                        modal.elm.remove();
+                    .appendTo('body');
+            
+                modal
+                    .css({
+                        position: 'fixed',
+                        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+            
+                        // 시꺼먼 레이어 보다 한칸 위에 보이기
+                        zIndex: zIndex + 1,
+            
+                        // div center 정렬
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        msTransform: 'translate(-50%, -50%)',
+                        webkitTransform: 'translate(-50%, -50%)'
+                    })
+                    .show()
+                    // 닫기 버튼 처리, 시꺼먼 레이어와 모달 div 지우기
+                    .find('.modal_close_btn')
+                    .on('click', function() {
+                        bg.remove();
+                        modal.hide();
                     });
-            },
-            error: function(res) {
-                if (opt.error) opt.error(res.responseText);
-                else {
-                    alert('error');
-                }
-                throw 'popup_error';
             }
-        });
-    };
-})(jQuery);
