e7a0d7f39730762776f7e7fc5ba35182a43a2673
yoonsangeun <yoonsangeun1@gmail.com>
2021-01-07 16:20:39 +0900

    공통함수 추가

--- ifg_front/static/common/common.js
+++ ifg_front/static/common/common.js
@@ -49,3 +49,52 @@ function retrieveCmmCd(sbx_id, grp_id){
              }
      });
  }
+
+/* 공통코드 조회 및 radio, select 컴포넌트 등록
+테스트 중 윤상은 */
+function retrieveCmmCdTest(cmp_id, grp_id){
+    var param = {
+        "cmp_id" : cmp_id,
+        "grp_id" : grp_id
+    }
+
+    $.ajaxCall( param, {
+        method : 'GET',
+            'url' : '/prj/retrieveCmmCd/',
+            'dataType' : 'json',
+            'data' : JSON.stringify(param),
+            'async' : false,
+            'callbackFn' : function(data){
+                for(var i = 0; i < data.length; i++){
+                    if($('#' + cmp_id).attr('type') == 'radio') {
+                        //var radio = $("<input type='radio' name='' value=''></input>");
+                        //$('#' + cmp_id).parent().append(radio);
+
+                        //윤상은 작업중 2020-01-07
+                    } else {
+                        var option = $("<option value = "+data[i].CMM_CD+">"+data[i].CMM_CD_NAME+"</option>");
+                        $('#' + cmp_id).append(option);
+                    }
+                }
+            }
+    });
+ }
+
+
+/* url로 넘긴 param 받기
+사용방법 : var value = $.urlParam("key"); //url 에서 get 으로 넘긴 parameter 'key' 받기
+*/
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+
+    if (results == null || results == '' ){
+        console.log("method=get::param= { " + name + " : null }");
+        return null;
+
+    } else{
+        console.log("method=get::param= { " + name + " : " + results[1] + " }");
+        return results[1] || 0;
+
+    }
+}
+
