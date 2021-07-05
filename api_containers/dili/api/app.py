c323e93fa9bb8e2a1af21320075f934149995f15
BabyFox2 <lovernd@naver.com>
2021-07-05 18:03:36 +0900

    IP수정

--- api_containers/dili/api/app.py
+++ api_containers/dili/api/app.py
@@ -47,7 +47,7 @@ def getSystemInfo():
     logging.debug('dill Server')
     logging.debug(socket.gethostbyname(socket.gethostname()))
     try:
-        if (socket.gethostbyname(socket.gethostname()) == "172.20.0.13" ) :
+        if (socket.gethostbyname(socket.gethostname()) == "172.20.0.5" ) :
             logging.debug('Prod Server')
             return "mariadb"
         else :
